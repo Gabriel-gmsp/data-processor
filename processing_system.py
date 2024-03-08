ef5343e3dbb8fa7563fbea1a5460411676e11acb
Gabriel Moreira <gabriel.gmsp@hotmail.com>
2024-03-08 13:59:54 -0300

    first commit

---
+++ processing_system.py
@@ -0,0 +1,39 @@
+import abc
+import time
+import csv
+import requests
+from mysql.connector import connect, Error
+
+# Definindo uma interface para fontes de dados
+class SourceInterface(metaclass=abc.ABCMeta): 
+    @abc.abstractmethod
+    def get_data(self):
+        pass
+
+class TargetInterface(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def save_data(self, data): # Método abstrato para salvar dados.
+        pass
+
+class DataProcessor(metaclass=abc.ABCMeta): #Classe abstrata para processamento de dados
+    def __init__(self, source, target):
+        
+        # Inicializa um objeto DataProcessor.
+        # Args:
+        #     source: Fonte de dados a ser processada.
+        #     target: Destino onde os dados processados serão salvos.
+        
+        self.source = source
+        self.target = target
+
+    def process(self): # Coordena o processo de processamento de dados
+        start_time = time.time()
+        data = self.source.get_data()
+        processed_data = self.process_data(data)
+        self.target.save_data(processed_data)
+        end_time = time.time()
+        print(f"{self.__class__.__name__} took {end_time - start_time} seconds to process.")
+
+    @abc.abstractmethod
+    def process_data(self, data):
+        pass
